a5fe3880178d76cd67ce39020c07f592491beba9
raphael.duarte <raphaeldpaiva@gmail.com>
2019-05-10 18:38:08 -0300

    Sharing x axis

--- ondas.py
+++ ondas.py
@@ -6,7 +6,7 @@ import numpy
 pi = numpy.pi
 sin = numpy.sin
 
-f = 100 # Hz
+f = 440 # Hz
 w = 2 * pi * f
 t = numpy.arange(0.0, 0.1, 0.0001)
 phase = 2
@@ -18,8 +18,7 @@ def generate_static_data():
 
   return baseline_data, mod_data, result_data
 
-def main():
-  baseline_data, mod_data, result_data = generate_static_data()
+def plot(baseline_data, mod_data, result_data):
   fig = plt.figure()
 
   base_graph = fig.add_subplot(211)
@@ -27,11 +26,16 @@ def main():
   baseline = base_graph.plot(t, baseline_data)
   gainline = base_graph.plot(t, mod_data)
 
-  result_graph = fig.add_subplot(212)
+  result_graph = fig.add_subplot(212, sharex=base_graph)
   resultline = result_graph.plot(t, result_data)
 
   plt.setp(gainline, color='g')
   plt.setp(resultline, color='r')
+
+def main():
+  baseline_data, mod_data, result_data = generate_static_data()
+
+  plot(baseline_data, mod_data, result_data)
 
   plt.show()
 
