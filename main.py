481b5e8dfa96e6862a61dbd55fdc65cdf7db3d7f
raphael.duarte <raphaeldpaiva@gmail.com>
2019-05-15 20:23:07 -0300

    writing wave files

--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 
 from sinusoud import Sinusoid
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
 
-wave_sample = Sinusoid(440).sample(1, 44100)
+frequency   = 440 #Hz
+duration    = 2.0 #Second
+sample_rate = 44100 #samples / Second
 
-plt.plot(wave_sample)
+signal = Sinusoid(frequency)
+wave_sample = signal.sample(duration, sample_rate)
+
+plt.plot(np.arange(0.0, duration, 1.0/sample_rate), wave_sample)
 plt.show()
+
+wavfile.write('sine.wav', sample_rate, wave_sample)
