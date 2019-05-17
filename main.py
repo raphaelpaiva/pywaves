304597c3d57f7c1344a2646653c1047bb0f08278
raphael.duarte <raphaeldpaiva@gmail.com>
2019-05-17 17:23:36 -0300

    Plotting real-time wave samples

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python
 
+import time
 from sinusoud import Sinusoid
+
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.io import wavfile
 
-frequency   = 440 #Hz
-duration    = 2.0 #Second
-sample_rate = 44100 #samples / Second
+from tkinter import TclError
 
-signal = Sinusoid(frequency)
-wave_sample = signal.sample(duration, sample_rate)
+frequency   = 440   # Hz
+sample_rate = 44100 # Samples / Second
+read_size   = 1024
+duration    = read_size / sample_rate # Seconds
 
-plt.plot(np.arange(0.0, duration, 1.0/sample_rate), wave_sample)
-plt.show()
+t = np.arange(0.0, duration, 1.0/sample_rate)
 
-wavfile.write('sine.wav', sample_rate, wave_sample)
+signal = Sinusoid(frequency, phase=1.0)
+
+fig, ax = plt.subplots(1)
+line, = ax.plot(t, np.random.rand(read_size), '-', lw=2)
+
+plt.show(block=False)
+
+frame_count = 0
+start_time = time.time()
+sample_frame_number = 0
+ax.set_ylim(-1,1)
+while True:
+  sample, t = signal.sample(duration, sample_rate, start_time=sample_frame_number)
+  line.set_data(t, sample)
+  ax.set_xlim(sample_frame_number, duration + sample_frame_number)
+  frame_count += 1
+  sample_frame_number += read_size
+
+  try:
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+  except TclError:
+    frame_rate = frame_count / (time.time() - start_time)
+    print(f"fps: {frame_rate}")
+    break
+
