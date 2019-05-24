700a050aca99168651df3c71bafe344922b264a2
Raphael Paiva <raphaeldpaiva@gmail.com>
2019-05-24 01:06:22 -0300

    Code cleanup

--- player.py
+++ player.py
@@ -3,37 +3,6 @@
 import pyaudio
 import numpy
 
-default_config = {
-  "channels":   1,
-  "read_size":   1024,
-  "sample_rate": 44100,
-  "duration":    1024 / 44100,
-  "format": pyaudio.paFloat32,
-  "volume": 0.5
-}
-
-def play(signal, config=default_config):
-  play_sinusoid(signal, config)
-
-def play_sinusoid(signal, config=default_config):
-  pa = pyaudio.PyAudio()
-  stream = pa.open(
-    channels = config["channels"],
-    format = config["format"],
-    rate=config["sample_rate"],
-    output=True
-  )
-
-  for t in range(88):
-    time = t * config["duration"]
-    sample, _ = signal.sample(config["duration"], config["sample_rate"], start_time=time)
-    normalized = numpy.interp(sample, (-1, 1), (0, config["volume"])).astype(numpy.float32)
-    
-    stream.write(normalized.tostring())
-
-  stream.close()
-  pa.terminate()
-
 class Player(object):
   def __init__(self, channels=1, sample_size=1024, sample_rate=44100, audio_format=pyaudio.paFloat32, volume=0.5):
     self.channels = channels
