e16992ccd348cc9d00901aaad214c3260e275266
Raphael Paiva <raphaeldpaiva@gmail.com>
2019-05-24 19:30:29 -0300

    Support for multiple oscilators and ttk interface.

--- interface.py
+++ interface.py
@@ -1,11 +1,11 @@
 import threading
 import tkinter
 
-from tkinter import Frame
-from tkinter import Label
-from tkinter import LabelFrame
-from tkinter import Scale
-from tkinter import Button
+from tkinter.ttk import Frame
+from tkinter.ttk import Label
+from tkinter.ttk import LabelFrame
+from tkinter.ttk import Scale
+from tkinter.ttk import Button
 
 from sinusoud import Sinusoid
 from oscilator import Oscilator
@@ -13,11 +13,14 @@ from player import Player
 
 class Window(Frame):
   def __init__(self, master=None):
-    Frame.__init__(self, master, padx=10, pady=5)
+    Frame.__init__(
+      self,
+      master
+    )
     self.master = master
     self.stop   = False
     
-    self.oscilator     = Oscilator()
+    self.oscilators    = [Oscilator(name="OSC1", wave=Sinusoid()), Oscilator(name="OSC2", wave=Sinusoid())]
     self.player        = Player()
     self.player_thread = threading.Thread(target=self._continuous_play)
     
@@ -28,49 +31,71 @@ class Window(Frame):
   def _create_window(self):
     self.master.title("JustASynth")
     
-    osc_frame = self._create_osc(self.oscilator)
-    osc_frame.grid()
+    osc_frames = self._create_osc()
+    for frame in osc_frames:
+      frame.grid()
 
     self.grid()
   
-  def _create_osc(self, osc):
-    osc_frame = LabelFrame(
-      self,
-      text=osc.name,
-      relief=tkinter.GROOVE,
-      borderwidth=5
-    )
-
+  def _create_osc(self):
+    osc_frames = []
     
-    freq_tracker = tkinter.IntVar()
-    freq_tracker.set(osc.wave.frequency)
+    for osc in self.oscilators:
+      osc_frame = LabelFrame(
+        self,
+        text=osc.name,
+        relief=tkinter.GROOVE,
+        borderwidth=5
+      )
+      
+      freq_tracker = tkinter.DoubleVar()
+      freq_tracker.set(float(osc.wave.frequency))
 
-    Scale(
-      osc_frame,
-      variable=freq_tracker,
-      label="Frequency",
-      from_=20,
-      to=5000,
-      orient=tkinter.HORIZONTAL,
-      command=osc.set_frequency,
-      length=400
-    ).grid(row=0, column=0)
+      Label(
+        osc_frame,
+        text="Frequency"
+      ).grid(row=1, column=0)
 
-    vol_tracker = tkinter.DoubleVar()
-    vol_tracker.set(self.player.volume)
+      Label(
+        osc_frame,
+        textvariable=freq_tracker
+      ).grid(row=0, column=1)
+      
+      Scale(
+        osc_frame,
+        variable=freq_tracker,
+        from_=20,
+        to=5000,
+        orient=tkinter.HORIZONTAL,
+        command=osc.set_frequency,
+        length=400
+      ).grid(row=1, column=1)
 
-    Scale(
-      osc_frame,
-      variable=vol_tracker,
-      label="Volume",
-      from_=0.0,
-      to=1.0,
-      orient=tkinter.HORIZONTAL,
-      command=osc.set_volume,
-      resolution=0.01
-    ).grid(row=1, column=0, sticky=tkinter.W)
+      vol_tracker = tkinter.DoubleVar()
+      vol_tracker.set(float(osc.volume))
 
-    return osc_frame
+      Label(
+        osc_frame,
+        text="Volume"
+      ).grid(row=3, column=0)
+
+      Label(
+        osc_frame,
+        textvariable=vol_tracker
+      ).grid(row=2, column=1)
+
+      Scale(
+        osc_frame,
+        variable=vol_tracker,
+        from_=0.0,
+        to=1.0,
+        orient=tkinter.HORIZONTAL,
+        command=osc.set_volume,
+      ).grid(row=3, column=1, sticky=tkinter.W)
+
+      osc_frames.append(osc_frame)
+    
+    return osc_frames
 
   
   def _continuous_play(self):
@@ -81,9 +106,15 @@ class Window(Frame):
       duration    = sample_size / sample_rate
       time        = t * duration
       
-      sample, _ = self.oscilator.wave.sample(duration, sample_rate, start_time=time)
+      samples = []
 
-      self.player.play_sample(sample)
+      for osc in self.oscilators:
+        sample, _ = osc.wave.sample(duration, sample_rate, start_time=time)
+        samples.append(sample)
+
+      master = self.player.mix(samples)
+
+      self.player.play_sample(master)
 
       t += 1
 
