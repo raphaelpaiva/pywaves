7b20dd529c01055d2651d220b6cb762be8b2cdef
Raphael Paiva <raphaeldpaiva@gmail.com>
2019-05-24 00:18:02 -0300

    Change frequency and play a single wave

--- interface.py
+++ interface.py
@@ -6,21 +6,21 @@ from tkinter import Scale
 from tkinter import Button
 
 from sinusoud import Sinusoid
-from player import play
+from player import Player
 
 class Window(Frame):
   def __init__(self, master=None):
     Frame.__init__(self, master)
     self.master = master
-    self.frequency = 440
+    self.sinusoid = Sinusoid()
+    self.player = Player()
     
     self._create_window()
-
 
   def _create_window(self):
     self.master.title("JustASynth")
     frequencyTracker = tkinter.IntVar()
-    frequencyTracker.set(self.frequency)
+    frequencyTracker.set(self.sinusoid.frequency)
 
     scale = Scale(
       self,
@@ -32,23 +32,34 @@ class Window(Frame):
       command=self._setFrequency
     )
     
-    scale.grid(row=0, column=1)
+    scale.grid(row=0, column=0)
 
     button = Button(self, text="Sample", command=self._sample)
-    button.grid(row=0, column=2)
+    button.grid(row=0, column=1)
 
     self.grid()
 
   def _sample(self):
-    play(Sinusoid(frequency=int(self.frequency)))
+    self.player.play_sample(self.sinusoid.sample(2, self.player.sample_rate))
   
   def _setFrequency(self, freq):
-    self.frequency = freq
+    self.sinusoid.frequency = int(freq)
+
+  def terminate(self):
+    self.player.terminate()
 
 
-root = tkinter.Tk()
+def main():
+  root = tkinter.Tk()
 
-root.geometry("400x300")
+  root.geometry("400x300")
 
-app = Window(root)
-root.mainloop()
+  app = Window(root)
+  
+  root.mainloop()
+
+  app.terminate()
+
+if __name__ == "__main__":
+  main()
+
--- player.py
+++ player.py
@@ -7,14 +7,19 @@ default_config = {
   "channels":   1,
   "read_size":   1024,
   "sample_rate": 44100,
-  "duration":    1024 / 44100
+  "duration":    1024 / 44100,
+  "format": pyaudio.paFloat32,
+  "volume": 0.5
 }
 
 def play(signal, config=default_config):
+  play_sinusoid(signal, config)
+
+def play_sinusoid(signal, config=default_config):
   pa = pyaudio.PyAudio()
   stream = pa.open(
     channels = config["channels"],
-    format = pyaudio.paFloat32,
+    format = config["format"],
     rate=config["sample_rate"],
     output=True
   )
@@ -22,9 +27,43 @@ def play(signal, config=default_config):
   for t in range(88):
     time = t * config["duration"]
     sample, _ = signal.sample(config["duration"], config["sample_rate"], start_time=time)
-    normalized = numpy.interp(sample, (-1, 1), (0, 1)).astype(numpy.float32)
+    normalized = numpy.interp(sample, (-1, 1), (0, config["volume"])).astype(numpy.float32)
     
     stream.write(normalized.tostring())
 
   stream.close()
   pa.terminate()
+
+class Player(object):
+  def __init__(self, channels=1, sample_size=1024, sample_rate=44100, audio_format=pyaudio.paFloat32, volume=0.5):
+    self.channels = channels
+    self.read_size = sample_size
+    self.sample_rate = sample_rate
+    self.format = audio_format
+    self.volume = volume
+    self.pyaudio = pyaudio.PyAudio()
+
+    self.stream = self.open_stream()
+
+  def open_stream(self):
+    stream = self.pyaudio.open(
+      channels = self.channels,
+      format = self.format,
+      rate=self.sample_rate,
+      output=True
+    )
+
+    return stream
+
+  def play_sample(self, sample):
+    normalized = numpy.interp(  # Normalize the sample to range(0, 1)
+      sample,
+      (-1, 1),                  # Original range from Sinewave
+      (0, self.volume)          # Target Range: 0 to Volume factor (1 is max)
+    ).astype(numpy.float32)
+    
+    self.stream.write(normalized.tostring())
+
+  def terminate(self):
+    self.stream.close()
+    self.pyaudio.terminate()
