6d5e621f4d7e2097e3a0069f5fa561b2295515cb
Raphael Paiva <raphaeldpaiva@gmail.com>
2019-05-24 00:38:04 -0300

    Live updates to continuous playback of one wave

--- interface.py
+++ interface.py
@@ -1,3 +1,4 @@
+import threading
 import tkinter
 
 from tkinter import Frame
@@ -12,10 +13,15 @@ class Window(Frame):
   def __init__(self, master=None):
     Frame.__init__(self, master)
     self.master = master
+    self.stop = False
+    
     self.sinusoid = Sinusoid()
     self.player = Player()
+    self.player_thread = threading.Thread(target=self._continuous_play)
     
     self._create_window()
+    
+    self.player_thread.start()
 
   def _create_window(self):
     self.master.title("JustASynth")
@@ -34,18 +40,27 @@ class Window(Frame):
     
     scale.grid(row=0, column=0)
 
-    button = Button(self, text="Sample", command=self._sample)
-    button.grid(row=0, column=1)
-
     self.grid()
-
-  def _sample(self):
-    self.player.play_sample(self.sinusoid.sample(2, self.player.sample_rate))
   
   def _setFrequency(self, freq):
     self.sinusoid.frequency = int(freq)
 
+  def _continuous_play(self):
+    t = 0
+    while not self.stop:
+      sample_size = self.player.sample_size
+      sample_rate = self.player.sample_rate
+      duration = sample_size / sample_rate
+      
+      sample, _ = self.sinusoid.sample(duration, sample_rate, start_time=t * duration)
+
+      self.player.play_sample(sample)
+
+      t += 1
+
   def terminate(self):
+    self.stop = True
+    self.player_thread.join()
     self.player.terminate()
 
 
