89e0fc0d63b4227395608ec9c8943e2a7ba00491
Raphael Paiva <raphaeldpaiva@gmail.com>
2019-05-24 19:29:51 -0300

    Bug: same sinusoid instance in every oscilator

--- oscilator.py
+++ oscilator.py
@@ -3,14 +3,14 @@
 from sinusoud import Sinusoid
 
 class Oscilator(object):
-  def __init__(self, name="OSC", wave=Sinusoid(), volume=1.0):
+  def __init__(self, wave, name="OSC", volume=1.0):
     self.name           = name
     self.wave           = wave
     self.volume         = volume
     self.wave.amplitude = volume
   
   def set_frequency(self, freq):
-    self.wave.frequency = int(freq)
+    self.wave.frequency = float(freq)
   
   def set_volume(self, vol):
     self.volume         = float(vol)
