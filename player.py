bef94c0d14687392bd63b0c87f95f4a2f1b31f98
raphaelpaiva <raphaeldpaiva@gmail.com>
2019-10-10 19:07:14 -0300

    Using sounddevice instead of pyaudio

--- player.py
+++ player.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 
-import pyaudio
+import sounddevice as sd
 import numpy
 
 class Player(object):
-  def __init__(self, channels=1, sample_size=1024, sample_rate=44100, audio_format=pyaudio.paFloat32, volume=0.5):
+  def __init__(self, channels=1, sample_size=1024, sample_rate=44100, audio_format=numpy.float32, volume=0.5):
     self.channels    = channels
     self.sample_size = sample_size
     self.sample_rate = sample_rate
     self.format      = audio_format
     self.volume      = volume
-    self.pyaudio     = pyaudio.PyAudio()
 
     self.stream = self.open_stream()
 
   def open_stream(self):
-    stream = self.pyaudio.open(
-      channels = self.channels,
-      format = self.format,
-      rate=self.sample_rate,
-      output=True
+    stream = sd.OutputStream(
+      channels= self.channels,
+      dtype= self.format,
+      samplerate =self.sample_rate
     )
+
+    stream.start()
 
     return stream
 
@@ -43,18 +43,17 @@ class Player(object):
   def play_sample(self, sample):
     normalized = self.normalize(sample)
 
-    self.stream.write(normalized.tostring())
+    self.stream.write(normalized)
 
   def normalize(self, sample):
     return numpy.interp(  # Normalize the sample to range(0, 1)
       sample,
       (-1, 1),            # Original range from Sinewave
       (0, self.volume)    # Target Range: 0 to Volume factor (1 is max)
-    ).astype(numpy.float32)
+    ).astype(self.format)
 
   def set_volume(self, vol):
     self.volume = float(vol)
 
   def terminate(self):
     self.stream.close()
-    self.pyaudio.terminate()
