9d607129a8449dd46995025fecc7fae40b12ae78
Raphael Paiva <raphaeldpaiva@gmail.com>
2019-11-08 16:59:05 -0300

    Adding value and value bounds to rotary encoder

--- rotary.py
+++ rotary.py
@@ -2,31 +2,44 @@ import tkinter as tk
 import math
 
 class Knob(tk.Frame):
-  def __init__(self, parent, radius, lock=False, max_angle=365, min_angle=0, **kwargs):
+  def __init__(self, parent, radius=25, lock=False, max_angle=360, min_angle=0, init_angle=None, step_ratio=1, min_value=0, max_value=360, show_label=True, label_fmt="{0:.2f}", **kwargs):
     size_adjust = 2
     diameter = 2 * radius
     size = diameter + size_adjust
 
     super().__init__(parent)
 
-    self.center = (self.winfo_rootx() + radius + size_adjust, self.winfo_rooty() + radius + size_adjust)
-    self.radius = radius
-    self.angle = 180 + 45
+    
     self.line = None
     self.min_angle = min_angle
     self.max_angle = max_angle
+    self.step_ratio = step_ratio
+    self.min_value = min_value
+    self.max_value = max_value
+    
+    self.center = (self.winfo_rootx() + radius + size_adjust, self.winfo_rooty() + radius + size_adjust)
+    self.radius = radius
+    self.angular_amplitude = max_angle - min_angle
+    self.value_amplitude = max_value - min_value
+    
+    self.label_fmt = label_fmt
+    self.text = tk.StringVar()
+    self.canvas = tk.Canvas(self, width=size, height=size)
+    
+    self.angle = init_angle if init_angle is not None else (self.min_angle + self.max_angle) / 2
+    self._update_value(self.angle)
 
-    if max_angle < 365 or self.min_angle > 0:
+    if max_angle < 360 or self.min_angle > 0:
       self.lock=True
     else:
       self.lock = lock
 
-    self.canvas = tk.Canvas(self, width=size, height=size)
+    
     self.canvas.pack()
-
-    self.text = tk.StringVar(value=self.angle)
-    self.label = tk.Label(self, textvariable=self.text, borderwidth=1, relief="solid")
-    self.label.pack()
+    
+    if show_label:
+      self.label = tk.Label(self, textvariable=self.text, borderwidth=1, relief="solid")
+      self.label.pack()
 
     self._draw_circle(**kwargs)
     self._draw_line(**kwargs)
@@ -62,17 +75,32 @@ class Knob(tk.Frame):
     if self.last_mouse_y is not None:
       dy = event.y - self.last_mouse_y
 
-      self.set_angle(self.angle + dy)
+      new_angle = self.angle + dy * self.step_ratio
+
+      self.set_angle(new_angle)
+      self._update_value(new_angle)
 
     self.last_mouse_y = event.y
+    self._draw_line()
+
+  def _update_value(self, new_angle):
+    angular_step  = new_angle - self.min_angle            # how far away is the new angle from the minimum?
+    angular_ratio = angular_step / self.angular_amplitude # how much of the total is this step?
+    new_value = self.value_amplitude * angular_ratio      # The value must vary at the same rate as the angle
+    
+    if new_value > self.max_value:
+      new_value = self.max_value
+    if new_value < self.min_value:
+      new_value = self.min_value
+    
+    self.value = new_value
+    self.text.set(self.label_fmt.format(self.value))
 
   def _release(self, event):
     self.last_mouse_y = None
 
   def set_angle(self, angle):
-    self.angle = max(self.min_angle, min(angle, self.max_angle)) if self.lock else angle % 365
-    self._draw_line()
-    self.text.set(self.angle)
+    self.angle = max(self.min_angle, min(angle, self.max_angle)) if self.lock else angle % (abs(self.max_angle) - abs(self.min_angle))
 
 def main():
   root = tk.Tk()
@@ -80,10 +108,10 @@ def main():
 
   r = 25
 
-  k1 = Knob(root, r, width=2)
+  k1 = Knob(root, 10, width=2, min_angle=-45, max_angle=225, max_value=100, min_value=0)
   k1.pack()
 
-  k2 = Knob(root, r, width=2, lock=True)
+  k2 = Knob(root, r, width=2, min_value=0, max_value=100, lock=True, show_label=False)
   k2.pack()
 
   root.mainloop()
